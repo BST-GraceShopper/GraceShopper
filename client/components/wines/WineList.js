9beb119277b7d31ee89b0324c55cfc7603356ec0
Lindsey Zurovchak <zurovchaklindsey@gmail.com>
2020-04-29 19:22:38 -0400

    wine component

--- client/components/wines/WineList.js
+++ client/components/wines/WineList.js
@@ -1,15 +1,32 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
+import Paper from '@material-ui/core/Paper'
+import CardActions from '@material-ui/core/CardActions'
+import CardContent from '@material-ui/core/CardContent'
+import {Typography} from '@material-ui/core'
 
 const WineList = ({wines}) => {
   console.log(wines)
   return (
-    <div>
-      <ul>
-        {wines.map(wine => {
-          return <li>{wine.vinter}</li>
-        })}
-      </ul>
+    <div style={{display: 'flex', flexWrap: 'wrap', justifyContent: 'center'}}>
+      {wines.map(wine => {
+        return (
+          <Paper
+            elevation={3}
+            style={{
+              width: 'calc(100%/4)',
+              height: 200,
+              margin: 20,
+              padding: 10,
+              display: 'flex',
+              flexDirection: 'column'
+            }}
+          >
+            <Typography>Vinter: {wine.vinter}</Typography>
+            <Typography>Vintage: {wine.vintage}</Typography>
+          </Paper>
+        )
+      })}
     </div>
   )
 }
