81ccd6c2dd0410f53b7c66b0907c5f35b4eae99e
Lindsey Zurovchak <zurovchaklindsey@gmail.com>
2020-04-29 19:37:13 -0400

    updated wine component

--- client/components/wines/WineList.js
+++ client/components/wines/WineList.js
@@ -1,7 +1,8 @@
+/* eslint-disable react/jsx-key */
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import Paper from '@material-ui/core/Paper'
-import CardActions from '@material-ui/core/CardActions'
+import {CardMedia, Card} from '@material-ui/core/'
 import CardContent from '@material-ui/core/CardContent'
 import {Typography} from '@material-ui/core'
 
@@ -11,20 +12,45 @@ const WineList = ({wines}) => {
     <div style={{display: 'flex', flexWrap: 'wrap', justifyContent: 'center'}}>
       {wines.map(wine => {
         return (
-          <Paper
+          <Card
             elevation={3}
             style={{
               width: 'calc(100%/4)',
-              height: 200,
               margin: 20,
               padding: 10,
               display: 'flex',
-              flexDirection: 'column'
+              flexDirection: 'column',
+              backgroundColor: 'black',
+              justifyContent: 'center'
             }}
           >
-            <Typography>Vinter: {wine.vinter}</Typography>
-            <Typography>Vintage: {wine.vintage}</Typography>
-          </Paper>
+            <CardMedia image={wine.image} style={{width: 200, height: 200}} />
+            <CardContent
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center'
+              }}
+            >
+              {[
+                'vintage',
+                'vinter',
+                'type',
+                'grape',
+                'region',
+                'price',
+                'inventory'
+              ].map(key => {
+                return (
+                  <Typography style={{color: 'white'}}>
+                    {key}: {wine[key]}
+                  </Typography>
+                )
+              })}
+              {/* <Typography style={{ color:'white'}}>Vinter: {wine.vinter}</Typography>
+              <Typography style={{ color:'white'}}>Vintage: {wine.vintage}</Typography> */}
+            </CardContent>
+          </Card>
         )
       })}
     </div>
--- script/seed.js
+++ script/seed.js
@@ -21,7 +21,9 @@ async function seed() {
       type: 'Red',
       grape: 'Cabernet Sauvignon',
       price: 500.0,
-      inventory: 2
+      inventory: 2,
+      image:
+        'https://media.istockphoto.com/photos/black-wine-bottle-with-an-empty-label-on-black-background-picture-id1074112266?k=6&m=1074112266&s=170667a&w=0&h=qoJq5vfF18Vt99t8zLUfKqFtIGxAzcq49u29t8Xn-tE='
     }),
     Wine.create({
       vinter: 'Vinter 2',
@@ -31,7 +33,9 @@ async function seed() {
       type: 'White',
       grape: 'Sauvignon Blanc',
       price: 700.0,
-      inventory: 1
+      inventory: 1,
+      image:
+        'https://media.istockphoto.com/photos/black-wine-bottle-with-an-empty-label-on-black-background-picture-id1074112266?k=6&m=1074112266&s=170667a&w=0&h=qoJq5vfF18Vt99t8zLUfKqFtIGxAzcq49u29t8Xn-tE='
     })
   ])
 
