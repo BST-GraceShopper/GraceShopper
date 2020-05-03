dbfe246eb2a95380c2b47f687e1404d7da000a51
Thomas Kassa <thomaskassajote@gmail.com>
2020-05-02 21:20:07 -0400

    ...

--- server/api/index.js
+++ server/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router()
 module.exports = router
 
+router.use('/', require('./order'))
 router.use('/users', require('./users'))
 router.use('/wines', require('./wines'))
+router.use('/spirits', require('./spirits'))
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,6 +1,9 @@
 const User = require('./user')
 const Wine = require('./wine')
-
+const Order = require('./order')
+const Product = require('./product')
+const Beer = require('./beer')
+const Spirit = require('./spirit')
 /**
  * If we had any associations to make, this would be a great place to put them!
  * ex. if we had another model called BlogPost, we might say:
@@ -14,7 +17,34 @@ const Wine = require('./wine')
  * for example, we can say: const {User} = require('../db/models')
  * instead of: const User = require('../db/models/user')
  */
+
+Order.belongsTo(User)
+Order.hasMany(Product)
+
+const addToCart = async (prodId, userId) => {
+  const {id, name, maker, price, image} = await Product.findByPk(prodId)
+  await Order.create({
+    userId,
+    productId: id,
+    name,
+    maker,
+    image,
+    price,
+    status: 'cart',
+    quantity: 1
+  })
+}
+const removeFromCart = async (productId, userId) => {
+  await Order.destroy({where: {userId, productId}})
+}
+
 module.exports = {
   User,
-  Wine
+  Wine,
+  Spirit,
+  Order,
+  Product,
+  Beer,
+  addToCart,
+  removeFromCart
 }
